94954c0b31b7bd37cac84ee1555788278b8eddd7
Keerthana-Sen <keesen23@gmail.com>
2026-09-23 13:58:23 +0530

    delete the extra vite

--- vite.config.tsx
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
-import react from "@vitejs/plugin-react";
-import agents from "agents/vite";
-
-export default defineConfig({
-  plugins: [agents(), react(), cloudflare()],
-});
